04b620cc0f6a1699179677c71e6c3f198552b22a
thejzain <thejzainkp@gmail.com>
2024-04-21 20:42:15 +0530

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::string::ParseError;
 
 fn main() {
     let mut sudoku: [[i8; 9]; 9] = [
@@ -14,56 +13,6 @@ fn main() {
         [0, 0, 5, 8, 9, 0, 4, 0, 7],
     ];
     println!("{:?}", validity_check(&sudoku));
-}
-
-fn validity_check_unfin(sudoku: &[[i8; 9]; 9]) -> Result<(), String> {
-    let mut check: HashMap<i8, bool> = HashMap::new();
-
-    //Checks all row for duplicates
-    for i in 0..9 {
-        for j in 0..9 {
-            if sudoku[i][j] == 0 {
-                continue;
-            }
-            match check.get(&sudoku[i][j]) {
-                Some(_) => {
-                    return Err(format!("Row check {}, {}", i, j));
-                }
-                None => {
-                    check.insert(sudoku[i][j], true);
-                }
-            }
-        }
-        check.clear();
-    }
-
-    //Checks all column for duplicates
-    for i in 0..9 {
-        for j in 0..9 {
-            if sudoku[j][i] == 0 {
-                continue;
-            }
-            match check.get(&sudoku[j][i]) {
-                Some(_) => {
-                    return Err(format!("Col check {}, {}", i, j));
-                }
-                None => {
-                    check.insert(sudoku[j][i], true);
-                }
-            }
-        }
-        check.clear();
-    }
-
-    //Check the 3x3 boxes
-    for i in 0..9 {
-        for j in 0..9 {
-            if sudoku[i][j] == 0 {
-                continue;
-            }
-        }
-    }
-    Ok(())
 }
 
 fn validity_check(sudoku: &[[i8; 9]; 9]) -> Result<(), String> {
